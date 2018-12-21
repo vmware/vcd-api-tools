648d5fbff8f7d5bc33c762da3bdadd16e34d73ea
sjones-vcd <11442451+sjones-vcd@users.noreply.github.com>
2018-12-21 11:35:24 -0500

    Add infrastructure for publishing release builds to Sonatype (#2)
    Removes hard-coded tagging of build from pom and add restriction for travis to only deploy tagged builds
    
    Signed-off-by: Steve Jones <sjones@vmware.com>

---
+++ build-resources/travis-release.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+#
+#  This script will upload release versions to Maven Central.
+#
+
+# Prerun the script to download all the dependent artifacts
+mvn help:evaluate -Dexpression=project.version
+
+current_pom_version=`mvn help:evaluate -Dexpression=project.version | grep -v '^\['`
+
+if [[ "${current_pom_version}" == *SNAPSHOT ]]
+then
+    echo "Snapshot version detected.  Not deployed to Sonatype"
+else
+    echo "Release version ${current_pom_version} detected"
+
+    # Capture any out-of-date dependencies to the log
+    mvn versions:display-dependency-updates
+
+fi
+
+# Push to Sonatype/Maven Central
+mvn --settings build-resources/travis-settings.xml deploy

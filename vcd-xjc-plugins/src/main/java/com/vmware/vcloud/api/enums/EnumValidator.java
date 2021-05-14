23676735575e39d0b626e17a70a5afce96db67df
ankitalode <43805898+ankitalode@users.noreply.github.com>
2021-05-14 18:42:34 +0530

    SDK v9.7 release (#29)
    Signed-off-by: Ankita Lode  <alode@vmware.com>
    
    Co-authored-by: ankitalode <alode@vmware.com>

---
+++ vcd-xjc-plugins/src/main/java/com/vmware/vcloud/api/enums/EnumValidator.java
@@ -0,0 +1,51 @@
+package com.vmware.vcloud.api.enums;
+
+/*-
+ * #%L
+ * vcd-xjc-plugins :: Custom plugins for XML to Java Compilation
+ * %%
+ * Copyright (C) 2018 - 2021 VMware, Inc.
+ * %%
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ * #L%
+ */
+
+import java.io.Serializable;
+
+import com.vmware.vcloud.api.rest.version.Supported;
+
+/**
+ * A simple validation interface; do not use outside the scope of special
+ * cases of {@code NameVersionHolder}
+ *
+ * @since 7.2
+ */
+public interface EnumValidator<T extends Supported> extends Serializable {
+
+    /**
+     * Perform validation on the key and value pair
+     *
+     * @param key
+     * @param value
+     */
+    void validate(String key, T value);
+}
